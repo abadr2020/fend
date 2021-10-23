e6bba890dc7d9dedafed28e4d8e36fbada04d021
abadr2020 <ahmed.badr@suezcanal.gov.eg>
2021-10-23 16:38:40 +0200

    Enhancing code, add features

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -23,19 +23,30 @@
  * 
 */
 let sections;
-let navDocFragment;
 let navBarList;
+const scrollToTopBtn = document.querySelector('#scrollToTop');
+let timer = null;
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function isSectionIsVisible(element){
-    let boundingRect = element.getBoundingClientRect();
-    let visible = boundingRect.bottom > 0 && boundingRect.top < window.innerHeight && boundingRect.top >= -1;
-    return visible;
-}
+// function isSectionIsVisible(section){
+//     let boundingRect = section.getBoundingClientRect();
+//     let visible = boundingRect.bottom > 0 && boundingRect.bottom < window.innerHeight && boundingRect.top < window.innerHeight && boundingRect.top >= -1;
+//     return visible;
+// }
 
+function hideNavBar(){
+    console.log(scrolled);
+    let currScrollPosition = window.pageYOffset;
+    if (prevScrollPosition == currScrollPosition) {
+        document.querySelector('#navbar__list').style.display = "none";
+    }else{
+        document.querySelector('#navbar__list').style.display = "block";
+    }
+    prevScrollPosition = currScrollPosition;
+}
 
 /**
  * End Helper Functions
@@ -46,14 +57,25 @@ function isSectionIsVisible(element){
 
 
 document.addEventListener('DOMContentLoaded', function(){
+    // Add section to page
+    addSection();
     // build the nav
     buildNavMenu();
 
     // Add class 'active' to section when near top of viewport
-    document.addEventListener('scroll', setActiveSection);
+    // document.addEventListener('scroll', setActiveSection);
+    createObserver();
 
     // Scroll to anchor ID using scrollTO event
+    navBarList = document.querySelector('#navbar__list');
     navBarList.addEventListener('click', scrollToSection, false);
+    // Manage show/ hide scroll to top button
+    document.addEventListener('scroll', function(){
+        showScrollToTop();
+        handleNavBarVisibility();
+    });
+    scrollToTopBtn.addEventListener('click', scrollToTop);
+    
 });
 
 /**
@@ -61,36 +83,49 @@ document.addEventListener('DOMContentLoaded', function(){
  * Begin Events
  * 
 */
-
+// Add section
+function addSection(){
+    sections = document.querySelectorAll('section');
+    main = document.querySelector('main');
+    let section = document.createElement('section');
+    section.setAttribute('id','section'+(sections.length + 1).toString());
+    section.setAttribute('data-nav','Section '+(sections.length + 1).toString());
+    let div = document.createElement('div');
+    div.classList.add('landing__container');
+    div.innerHTML = `        
+    <h2>${section.dataset.nav}</h2>
+    <p>Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. 
+    Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. 
+    Sed convallis sollicitudin mauris ac tincidunt. 
+    Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. 
+    Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+    <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+    `;
+    section.append(div);
+    main.appendChild(section);
+}
 // Build menu 
 function buildNavMenu(){
-    // get HTMLCollection of all elements with section Tag Name
+// get nav Bar List Items
+navBarList = document.querySelector('#navbar__list');
+// get NodeList of all elements with section Tag Name
 sections = document.querySelectorAll('section');
 // create new Fragment
-navDocFragment = document.createDocumentFragment();
-// get ul of navbar list
-navBarList = document.querySelector('#navbar__list');
-// iterate over the HTMLCollection
-sections.forEach(function(section){
-    // create list item
-    let li = document.createElement('li');
-    // create anchor
-    let a = document.createElement('a');
-    // populate its HTML content with the value of the data-nav attribute
-    a.innerHTML = section.dataset.nav;
-    // add class to anchor
-    a.classList.add('menu__link');
-    //add href to anchor
-    a.setAttribute('href', '#'+section.id);
-    // set data-nav of anchor to section Id
-    a.setAttribute('data-nav',section.id);
-    // a.setAttribute('data-order', i);
-    //append anchor to li
-    li.appendChild(a);
-    // append the list item to the navDocFragment
-    navDocFragment.appendChild(li);
-});
-// append Fragment to nav bar list
+let navDocFragment = document.createDocumentFragment();
+
+    sections.forEach(function(section){
+        let li = document.createElement('li');
+        let a = document.createElement('a');
+        a.innerHTML = section.dataset.nav;
+        a.classList.add('menu__link');
+        a.setAttribute('href', '#'+section.id);
+        // set data-nav of anchor to section Id
+        a.setAttribute('data-nav',section.id);
+        a.setAttribute('id','nav_'+section.id);
+        li.appendChild(a);
+        navDocFragment.appendChild(li);
+    });
 navBarList.appendChild(navDocFragment);
 }
 
@@ -100,23 +135,105 @@ function scrollToSection(e){
     if (e.target && e.target.nodeName.toLowerCase() == 'a'){
         // prevent default behavior of clicking the anchor (navigate)
         e.preventDefault();
+        // remove active class from all nav items
+        document.querySelectorAll('.active-class').forEach((element) => {
+            element.classList.remove('active-class');
+        })
+        // set active class to the clicked nav item
+        document.querySelector('#'+e.target.id).classList.add('active-class');
         // get section
         let section = document.querySelector('#'+e.target.dataset.nav);
-        window.scrollTo({
-            top: section.offsetTop,
-            behavior: 'smooth',
-          })
+        section.scrollIntoView({behavior: 'smooth'});
+        // window.scrollTo({
+        //     top: section.offsetTop,
+        //     behavior: 'smooth',
+        //   })
 
     }
 }
 // Set sections as active
-function setActiveSection(){
-    sections.forEach(function(section){
-        if(isSectionIsVisible(section)){
-            section.classList.add('your-active-class');
-         }else{
-            section.classList.remove('your-active-class');
-        }
-    });
+function createObserver(){
+    let observer;
+    // set observer options
+    let options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+  };
+  // declare observer with observer callback
+  observer = new IntersectionObserver(setIntersectedToActive, options);
+  // get sections
+  sections = document.querySelectorAll('section');
+  // observe each section
+  sections.forEach(function(section){
+  observer.observe(section);
+  });
 }
+// observer callback
+function setIntersectedToActive(entries){
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            // reomve your-active-class from all sections
+            document.querySelectorAll('.your-active-class').forEach((el) => {
+                    el.classList.remove('your-active-class');
+            });
+            // reomve active-class from all navItems
+            document.querySelectorAll('.active-class').forEach((el) => {
+                el.classList.remove('active-class');
+            });
+            // set section to target of intersection entry
+            let section = entry.target;
+            // set your-active-class to intersecting section
+            section.classList.add('your-active-class');
+            // get navItem
+            let navItem = document.querySelector('#nav_' + section.id);
+            // set active-class to nacItem of intersecting section
+            navItem.classList.add('active-class');
+            }
+      });
+}
+function showScrollToTop(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        scrollToTopBtn.setAttribute('style', 'display: block');
+      } else {
+        scrollToTopBtn.setAttribute('style', 'display: none');
+      }
+    }
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
+}
+function handleNavBarVisibility(){    
+    if(timer !== null) {
+        clearTimeout(timer);
+        navBarList.setAttribute('style','display:block');        
+    }
+    timer = setTimeout(function() {
+        if (document.body.scrollTop !== 0 || document.documentElement.scrollTop !== 0) {
+          navBarList.setAttribute('style','display:none');
+        }
+    }, 5000);
+}
+// function setActiveSection(){
+//     // Hide nav bar while not scrolling
+//     // setTimeout(function(){hideNavBar()},4000);
+//     sections = document.querySelectorAll('section');
+//     sections.forEach(function(section){
+//         if(isSectionIsVisible(section)){
+//             // set active class to section
+//             section.classList.add('your-active-class');
+//             // set active class to nav item
+//             document.querySelector('#nav_'+section.id).classList.add('active-class');
+//          }else{
+//             // remove active class to section
+//             section.classList.remove('your-active-class');
+//             // set active class to nav item
+//             document.querySelector('#nav_'+section.id).classList.remove('active-class');
+//         }
+//     });
+// }
+
 
